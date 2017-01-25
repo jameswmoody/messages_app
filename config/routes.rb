67477463aee68fe00d93a3ce1a3c76213d65c0d5
James W Moody <jamesmoodyradio@gmail.com>
2017-01-25 12:07:50 -0600

    refine login functionality

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'sessions/new'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'messages#index'
+  root 'sessions#new'
   resources :messages
   get  '/signup',  to: 'users#new'
   post '/signup',  to: 'users#create'
